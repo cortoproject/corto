bba44065a44baa1dec6974fbaa6e88837e22f59e
Johnny Lee <jleeothon@gmail.com>
2014-12-21 20:34:44 -0500

    #16 Added (commented) tests for char objects

--- test/tc_jsonser/src/tc_jsonser_primitive_value.c
+++ test/tc_jsonser/src/tc_jsonser_primitive_value.c
@@ -59,6 +59,12 @@ db_int16 test_ser_primitive_value(void) {
     _test_ser_primitive_value(bt, "true");
     _test_ser_primitive_value(bf, "false");
 
+    // _test_ser_primitive_value(c1, "\"a\"");
+    // _test_ser_primitive_value(c2, "\"A\"");
+    // _test_ser_primitive_value(c3, "\"0\"");
+    // _test_ser_primitive_value(c4, "\"\\n\"");
+    // _test_ser_primitive_value(c5, "\"\\0\"");
+
     _test_ser_primitive_value(sn, "null");
     _test_ser_primitive_value(s1, "\"hello world\"");
     _test_ser_primitive_value(s2, "\"hello, world!\"");
