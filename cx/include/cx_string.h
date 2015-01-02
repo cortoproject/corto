361974a08c9d881f79867ba47a282a58cbf56012
Johnny Lee <jleeothon@gmail.com>
2015-01-02 18:49:43 -0500

    Solving merge conflicts

--- cx/include/cx_string.h
+++ cx/include/cx_string.h
@@ -1,207 +1,50 @@
+#ifndef DB_STRING_H_
+#define DB_STRING_H_
+
+#include "string.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int stricmp(const char *str1, const char *str2);
+void strtoupper(char *ch);
+void strtolower(char *ch);
+char *strappend(char *src, char *fmt, ...);
+char *itostr(int value, char *result, int base);
+char *utostr(unsigned int value, char *result, int base);
+
 /*
- * db_string.c
- *
- *  Created on: Dec 27, 2012
- *      Author: sander
+ * Escapes a character `in` and prints it to `out`. Does not not include
+ * surrounding single quotes.
+ * Returns the next available location to write, usually out+1 or out+2.
  */
+char *schresc(char in, char *out, int isstr);
 
-#include "stdarg.h"
-#include "db_string.h"
-#include "db_mem.h"
-
-static int isesc(char c) {
-    char escapeSequences[] = "\a\b\f\n\r\t\v\"\\";
-    char *p = escapeSequences;
-    char d;
-    while ((d = *p++)) {
-        if (c == d) {
-            return 1;
-        }
-    }
-    return 0;
-}
-
-int stricmp(const char *str1, const char *str2) {
-    return strcasecmp(str1, str2);
-}
-
-void strtoupper(char *ch) {
-    if ((*ch >= 97) && (*ch <= 122)) {
-        *ch -= 32;
-    }
-}
-
-void strtolower(char *ch) {
-    if ((*ch >= 65) && (*ch <= 90)) {
-        *ch += 32;
-    }
-}
-
-char *strappend(char *src, char *fmt, ...) {
-    char buff[1024];
-    va_list args;
-    unsigned int sourceLength = 0;
-
-    va_start(args, fmt);
-    vsprintf(buff, fmt, args);
-    va_end(args);
-
-    if (src) {
-        sourceLength = strlen(src);
-    }
-
-    src = db_realloc(src, sourceLength + strlen(buff) + 1);
-    strcpy(&src[sourceLength], buff);
-
-    return src;
-}
-
-/**
- * C++ version 0.4 char* style "itoa":
- * Written by Lukás Chmela
- * Released under GPLv3.
+/*
+ * Wraps around `schresc`
  */
-char *itostr(int value, char *result, int base) {
-    // check that the base if valid
-    if (base < 2 || base > 36) { *result = '\0'; return result; }
+char *chresc(char in, char *out);
 
-    char* ptr = result, *ptr1 = result, tmp_char;
-    int tmp_value;
+/*
+ * Escapes a null-terminated string `in` and attempts to print it to `out`.
+ * Does not include surrounding double quotes.
+ * The maximum number of characters to be printed is `n` including the null
+ * character, but it may be less if the following character requires escaping.
+ * The recommended length to allocate for the buffer can be obtained with
+ * `stresclen`, adding 2 if you surrounding double quotes.
+ * The resulting string is null terminated.
+ */
+char *stresc(const char *in, char *out, size_t n);
 
-    do {
-        tmp_value = value;
-        value /= base;
-        *ptr++ = "zyxwvutsrqponmlkjihgfedcba9876543210123456789abcdefghijklmnopqrstuvwxyz" [35 + (tmp_value - value * base)];
-    } while ( value );
+/*
+ * Returns the length-to-be of a null-terminated string as if escaped with
+ * stresc.
+ */
+size_t stresclen(const char *s);
 
-    // Apply negative sign
-    if (tmp_value < 0) *ptr++ = '-';
-    *ptr-- = '\0';
-    while(ptr1 < ptr) {
-        tmp_char = *ptr;
-        *ptr--= *ptr1;
-        *ptr1++ = tmp_char;
-    }
-    return result;
+#ifdef __cplusplus
 }
+#endif
 
-char *utostr(unsigned int value, char *result, int base) {
-    // check that the base if valid
-    if (base < 2 || base > 36) { *result = '\0'; return result; }
-
-    char* ptr = result, *ptr1 = result, tmp_char;
-    int tmp_value;
-
-    do {
-        tmp_value = value;
-        value /= base;
-        *ptr++ = "zyxwvutsrqponmlkjihgfedcba9876543210123456789abcdefghijklmnopqrstuvwxyz" [35 + (tmp_value - value * base)];
-    } while ( value );
-
-    *ptr-- = '\0';
-    while(ptr1 < ptr) {
-        tmp_char = *ptr;
-        *ptr--= *ptr1;
-        *ptr1++ = tmp_char;
-    }
-    return result;
-}
-
-
-char *schresc(char in, char *out, int isstr) {
-    char *bptr = out;
-    switch(in) {
-    case '\a':
-        *bptr++ = '\\';
-        *bptr = 'a';
-        break;
-    case '\b':
-        *bptr++ = '\\';
-        *bptr = 'b';
-        break;
-    case '\f':
-        *bptr++ = '\\';
-        *bptr = 'f';
-        break;
-    case '\n':
-        *bptr++ = '\\';
-        *bptr = 'n';
-        break;
-    case '\r':
-        *bptr++ = '\\';
-        *bptr = 'r';
-        break;
-    case '\t':
-        *bptr++ = '\\';
-        *bptr = 't';
-        break;
-    case '\v':
-        *bptr++ = '\\';
-        *bptr = 'v';
-        break;
-    case '\\':
-        *bptr++ = '\\';
-        *bptr = '\\';
-        break;
-    case '\'':
-        if (!isstr) {
-            *bptr++ = '\\';
-        }
-        *bptr = '\'';
-        break;
-    case '"':
-        if (isstr) {
-            *bptr++ = '\\';
-        }
-        *bptr = '"';
-        break;
-    default:
-        *bptr = in;
-        break;
-    }
-
-    bptr++;
-
-    return bptr;
-}
-
-char *chresc(char in, char *out) {
-    return schresc(in, out, 0);
-}
-
-char *stresc(const char *in, char *out, size_t n) {
-    const char *p = in;
-    char *q = out;
-    char c;
-    size_t i = n;
-
-    while ((c = *p++)) {
-        if (n) {
-            if (isesc(c)) {
-                if (i <= 2) {
-                    break;
-                }
-                i -= 2;
-            } else {
-                if (i <= 1) {
-                    break;
-                }
-                i--;
-            }
-        }
-        q = schresc(c, q, 1);
-    }
-    *q = '\0';
-    return q;
-}
-
-size_t stresclen(const char *s) {
-    size_t count = 0;
-    const char *p = s;
-    char c;
-    while ((c = *p++)) {
-        count += isesc(c) ? 2 : 1;
-    }
-    count++;
-    return count;
-}
+#endif /* DB_STRING_H_ */
--- cx/src/cx_string.c
+++ cx/src/cx_string.c
@@ -1,25 +1,30 @@
-/*
- * cx_string.c
- *
- *  Created on: Dec 27, 2012
- *      Author: sander
- */
 
 #include "stdarg.h"
-#include "cx_string.h"
 #include "cx_mem.h"
 
-int stricmp(const char* str1, const char* str2) {
+static int isesc(char c) {
+    char escapeSequences[] = "\a\b\f\n\r\t\v\"\\";
+    char *p = escapeSequences;
+    char d;
+    while ((d = *p++)) {
+        if (c == d) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+int stricmp(const char *str1, const char *str2) {
     return strcasecmp(str1, str2);
 }
 
-void strtoupper(char* ch) {
+void strtoupper(char *ch) {
     if ((*ch >= 97) && (*ch <= 122)) {
         *ch -= 32;
     }
 }
 
-void strtolower(char* ch) {
+void strtolower(char *ch) {
     if ((*ch >= 65) && (*ch <= 90)) {
         *ch += 32;
     }
@@ -49,7 +54,7 @@ char *strappend(char *src, char *fmt, ...) {
  * Written by Lukás Chmela
  * Released under GPLv3.
  */
-char *itostr(int value, char* result, int base) {
+char *itostr(int value, char *result, int base) {
     // check that the base if valid
     if (base < 2 || base > 36) { *result = '\0'; return result; }
 
@@ -73,7 +78,7 @@ char *itostr(int value, char* result, int base) {
     return result;
 }
 
-char *utostr(unsigned int value, char* result, int base) {
+char *utostr(unsigned int value, char *result, int base) {
     // check that the base if valid
     if (base < 2 || base > 36) { *result = '\0'; return result; }
 
@@ -95,54 +100,101 @@ char *utostr(unsigned int value, char* result, int base) {
     return result;
 }
 
-char *stresc(char in, char* out, char delimiter) {
-    char *bptr = out;
 
+char *schresc(char in, char *out, int isstr) {
+    char *bptr = out;
     switch(in) {
     case '\a':
-        *bptr = '\\';
-        *(++bptr) = 'a';
+        *bptr++ = '\\';
+        *bptr = 'a';
         break;
     case '\b':
-        *bptr = '\\';
-        *(++bptr) = 'b';
+        *bptr++ = '\\';
+        *bptr = 'b';
         break;
     case '\f':
-        *bptr = '\\';
-        *(++bptr) = 'f';
+        *bptr++ = '\\';
+        *bptr = 'f';
         break;
     case '\n':
-        *bptr = '\\';
-        *(++bptr) = 'n';
+        *bptr++ = '\\';
+        *bptr = 'n';
         break;
     case '\r':
-        *bptr = '\\';
-        *(++bptr) = 'r';
+        *bptr++ = '\\';
+        *bptr = 'r';
         break;
     case '\t':
-        *bptr = '\\';
-        *(++bptr) = 't';
+        *bptr++ = '\\';
+        *bptr = 't';
         break;
     case '\v':
-        *bptr = '\\';
-        *(++bptr) = 'a';
+        *bptr++ = '\\';
+        *bptr = 'v';
         break;
     case '\\':
+        *bptr++ = '\\';
         *bptr = '\\';
-        *(++bptr) = '\\';
+        break;
+    case '\'':
+        if (!isstr) {
+            *bptr++ = '\\';
+        }
+        *bptr = '\'';
+        break;
+    case '"':
+        if (isstr) {
+            *bptr++ = '\\';
+        }
+        *bptr = '"';
         break;
     default:
-        if (in == delimiter) {
-            *bptr = '\\';
-            *(++bptr) = delimiter;
-        } else {
-            *bptr = in;
-        }
+        *bptr = in;
         break;
     }
+
     bptr++;
 
     return bptr;
 }
 
+char *chresc(char in, char *out) {
+    return schresc(in, out, 0);
+}
 
+char *stresc(const char *in, char *out, size_t n) {
+    const char *p = in;
+    char *q = out;
+    char c;
+    size_t i = n;
+
+    while ((c = *p++)) {
+        if (n) {
+            if (isesc(c)) {
+                if (i <= 2) {
+                    break;
+                }
+                i -= 2;
+            } else {
+                if (i <= 1) {
+                    break;
+                }
+                i--;
+            }
+        }
+        q = schresc(c, q, 1);
+    }
+    *q = '\0';
+    return q;
+}
+
+size_t stresclen(const char *s) {
+    size_t count = 0;
+    const char *p = s;
+    char c;
+    while ((c = *p++)) {
+        count += isesc(c) ? 2 : 1;
+    }
+    count++;
+    return count;
+}
