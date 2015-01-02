c1ed07a3896e9dfc50f73fab0cde4f82eb4451e7
Johnny Lee <jleeothon@gmail.com>
2015-01-02 18:55:28 -0500

    Adding more files for JSON serializer test

---
+++ test/tc_jsonser/src/tc_jsonser_fixture__load.c
@@ -0,0 +1,13 @@
+/** 
+ * This file is fully generated. Do not modify!
+ */
+
+#include "tc_jsonser_fixture__meta.h"
+
+/* This function is the entrypoint for the library and * loads definitions of the 'tc_jsonser_fixture' scope */
+int cortexmain(int argc, char* argv[]) {
+    CX_UNUSED(argc);
+    CX_UNUSED(argv);
+    return tc_jsonser_fixture_load();
+}
+
