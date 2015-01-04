d27459d0d5b488774f041afce83ca2d8821654dc
Johnny Lee <jleeothon@gmail.com>
2015-01-04 15:09:32 -0500

    #111 Replaced use of fprintf for g_fileWrite

--- generator/html/include/html.h
+++ generator/html/include/html.h
@@ -1,7 +1,9 @@
+#include "cx_generator.h"
 #include "cx_object.h"
 
 typedef struct cx_html_gen {
     const char *path;
     unsigned int level;
     const char *rootFullname;
+    cx_generator generator;
 } cx_html_gen_t;
