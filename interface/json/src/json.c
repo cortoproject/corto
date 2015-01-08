cb82295e3f4dd94ed6a1e05a1eb9392821c484ff
Johnny Lee <jleeothon@gmail.com>
2015-01-08 00:19:40 -0500

    #102 Restored apparently deleted bug fix for void objects

--- interface/json/src/json.c
+++ interface/json/src/json.c
@@ -439,16 +439,18 @@ static cx_int16 cx_ser_object(cx_serializer s, cx_value* v, void* userData) {
     }
 
     if (data->serializeValue) {
-        if (c && !cx_ser_appendstr(data, ",")) {
-            goto finished;
-        }
-        if (!cx_ser_appendstr(data, "\"value\":")) {
-            goto finished;
-        }
-        if (cx_serializeValue(s, v, userData)) {
-            goto error;
-        }
-        c += 1;
+        if (cx_valueType(v)->real->kind != CX_VOID) {
+            if (c && !cx_ser_appendstr(data, ",")) {
+                goto finished;
+            }
+            if (!cx_ser_appendstr(data, "\"value\":")) {
+                goto finished;
+            }
+            if (cx_serializeValue(s, v, userData)) {
+                goto error;
+            }
+            c += 1;
+         }
     }
 
     if (data->serializeScope) {
