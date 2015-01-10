628b83cc2dac6aa1be2024358aa8473d9c2b07b2
SanderMertens <sander.mertens8@gmail.com>
2015-01-10 13:36:41 -0500

    #12 Fixed invalid copy call in Fast::Object
    Code assumed that an object to object copy was performed, when this is
    not guaranteed.

--- interface/fast/src/Fast_Object.c
+++ interface/fast/src/Fast_Object.c
@@ -56,7 +56,7 @@ cx_int16 Fast_Object_serialize(Fast_Object _this, cx_type dstType, cx_word dst) 
     if (!dstType->reference) {
         cx_bool srcIsDelegate = FALSE, dstIsDelegate = FALSE;
         cx_object obj = Fast_ObjectBase(_this)->value;
-        cx_type srcType = cx_typeof(obj);
+        cx_type srcType = cx_typeof(obj)->real;
 
         /* Handle delegates */
         if ((srcType->kind == CX_COMPOSITE) && (cx_interface(srcType)->kind == CX_PROCPTR)) {
@@ -68,7 +68,10 @@ cx_int16 Fast_Object_serialize(Fast_Object _this, cx_type dstType, cx_word dst) 
 
         if (dstIsDelegate) {
             if (srcIsDelegate) {
-                cx_copy((cx_object)dst, Fast_ObjectBase(_this)->value);
+                cx_value vDst, vSrc;
+                cx_valueValueInit(&vDst, NULL, cx_typedef(dstType), (void *)dst);
+                cx_valueValueInit(&vSrc, NULL, cx_typedef(srcType), Fast_ObjectBase(_this)->value);
+                cx_valueCopy(&vDst, &vSrc);
             } else if ((srcType->kind == CX_COMPOSITE) && (cx_interface(srcType)->kind == CX_PROCEDURE)) {
                 cx_set(&((cx_procptrdata *)dst)->procedure, Fast_ObjectBase(_this)->value);
                 cx_set(&((cx_procptrdata *)dst)->instance, NULL);
@@ -76,7 +79,10 @@ cx_int16 Fast_Object_serialize(Fast_Object _this, cx_type dstType, cx_word dst) 
 
         } else if (cx_instanceof((cx_typedef)dstType, Fast_ObjectBase(_this)->value)) {
             /* If object is not of a reference type and object is of dstType, copy value */
-            cx_copy((cx_object)dst, Fast_ObjectBase(_this)->value);
+            cx_value vDst, vSrc;
+            cx_valueValueInit(&vDst, NULL, cx_typedef(dstType), (void *)dst);
+            cx_valueValueInit(&vSrc, NULL, cx_typedef(srcType), obj);
+            cx_valueCopy(&vDst, &vSrc);
 
         } else {
             cx_id id, id2;
