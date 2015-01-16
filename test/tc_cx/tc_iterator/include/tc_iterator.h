38acac25002b3bb95a9470de7c403da0c9684df6
Johnny Lee <jleeothon@gmail.com>
2015-01-16 13:38:05 -0500

    #14 Corrected iteration over arrays and added tests

---
+++ test/tc_cx/tc_iterator/include/tc_iterator.h
@@ -0,0 +1,27 @@
+/* tc_iterator.h
+ *
+ * This file contains generated code. Do not modify!
+ */
+
+#ifndef tc_iterator_H
+#define tc_iterator_H
+
+#include "cortex.h"
+#include "tc_iterator__type.h"
+
+#include "tc_iterator__api.h"
+
+#include "tc_iterator__meta.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* ::tc_iterator::testArray() */
+cx_bool tc_iterator_testArray(void);
+
+#ifdef __cplusplus
+}
+#endif
+#endif
+
