69cab166b65dee3ff86514575f08f5463861daef
SanderMertens <sander.mertens8@gmail.com>
2015-04-01 14:22:14 -0400

    #205 Separate dependency algorithm fro dependency resolver
    Fixes #205

---
+++ cx/include/cx_depresolver.h
@@ -0,0 +1,25 @@
+
+
+#ifndef CX__DEPRESOLVER_H_
+#define CX__DEPRESOLVER_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct cx_depresolver* cx_depresolver;
+
+typedef int (*cx_depresolver_action)(cx_object o, void *userData);
+
+cx_depresolver cx_depresolver__create(cx_depresolver_action onDeclare, cx_depresolver_action onDefine, void *userData);
+
+void cx_depresolver_depend(cx_depresolver _this, void *dependent, cx_uint8 kind, void *dependency, cx_uint8 dependencyKind);
+
+int cx_depresolver_walk(cx_depresolver _this);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
