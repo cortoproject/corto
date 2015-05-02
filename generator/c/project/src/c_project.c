ad1bb9ea8747eeb279ccc69e62aa21fe93bc0337
SanderMertens <sander.mertens8@gmail.com>
2015-05-02 17:48:06 -0400

    #233 Modify C project generator to support dependencies

--- generator/c/project/src/c_project.c
+++ generator/c/project/src/c_project.c
@@ -3,6 +3,7 @@
 #include "cx_generator.h"
 #include "cx_files.h"
 #include "cx_file.h"
+#include "c_common.h"
 
 /* Generate file containing component loader */
 static cx_int16 c_projectGenerateMainFile(cx_generator g) {
@@ -68,15 +69,54 @@ static cx_int16 c_projectGenerateMakefile(cx_generator g) {
     g_file file;
 
     /* Only generate makefile when no makefile is present */
-    if(!cx_fileTest("makefile")) {
-        file = g_fileOpen(g, "makefile");
+    if(!cx_fileTest("rakefile")) {
+        file = g_fileOpen(g, "rakefile");
         if(!file) {
             goto error;
         }
 
         g_fileWrite(file, "\n");
-        g_fileWrite(file, "TARGET = %s\n\n", g_getName(g));
-        g_fileWrite(file, "include $(CORTEX_HOME)/build/package.makefile\n\n");
+        g_fileWrite(file, "TARGET = :%s\n\n", g_getName(g));
+        g_fileWrite(file, "require \"#{ENV['CORTEX_HOME']}/build/package\"\n\n");
+    }
+
+    return 0;
+error:
+    return -1;
+}
+
+/* Generate dependency makefile for project */
+static cx_int16 c_projectGenerateDepMakefile(cx_generator g) {
+    g_file file;
+    cx_iter iter;
+
+    file = g_fileOpen(g, "dep.rb");
+    if(!file) {
+        goto error;
+    }
+
+    g_fileWrite(file, "\n");
+    g_fileWrite(file, "# include paths for dependent packages\n");
+
+    /* Add include path for dependent packages */
+    g_resolveImports(g);
+    if (g->imports) {
+        iter = cx_llIter(g->imports);
+        while (cx_iterHasNext(&iter)) {
+            cx_object import;
+            cx_id toRoot, path;
+            cx_object o = g_getCurrent(g);
+            *toRoot = '\0';
+            while (o) {
+                o = cx_parentof(o);
+                strcat(toRoot, "../");
+            }
+            import = cx_iterNext(&iter);
+            c_topath(import, path);
+            g_fileWrite(file, "INCLUDE << \"%spackages/%s/include\"\n", toRoot, path, cx_nameof(import));
+            g_fileWrite(file, "LIBPATH << \"%spackages/%s/bin\"\n", toRoot, path, cx_nameof(import));
+            g_fileWrite(file, "CORTEX_LIB << \"%s\"\n", cx_nameof(import));
+        }
     }
 
     return 0;
@@ -95,6 +135,9 @@ cx_int16 cortex_genMain(cx_generator g) {
         goto error;
     }
     if(c_projectGenerateMakefile(g)) {
+        goto error;
+    }
+    if(c_projectGenerateDepMakefile(g)) {
         goto error;
     }
 
