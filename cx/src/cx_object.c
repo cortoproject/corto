44d3ce8289e4e695da3aa3dfa5eb909a521a048f
SanderMertens <sander.mertens8@gmail.com>
2015-05-12 18:25:39 -0700

    #246 fixed destruct code

--- cx/src/cx_object.c
+++ cx/src/cx_object.c
@@ -1082,14 +1082,8 @@ void cx_destruct(cx_object o) {
     scope = cx__objectScope(_o);
 
     if (cx_ainc(&scope->orphaned) == 1) {
-        cx_bool builtin = cx_isbuiltin(o);
-        if (!builtin) {
-            cx__destruct(o);
-        }
+        cx_notify(cx__objectObservable(_o), o, o, CX_ON_DELETE);
         cx__orphan(o);
-        if (!builtin) {
-            cx__deinitScope(o);
-        }
         cx_free_ext(cx_parentof(o), o, "destroy scope reference");
     }
 }
@@ -1488,15 +1482,14 @@ cx_uint16 cx__destruct(cx_object o) {
                 }
             }
 
-            /* Notify destruct */
-            cx_notify(cx__objectObservable(CX_OFFSET(o,-sizeof(cx__object))), o, o, CX_ON_DELETE);
-
             /* Call object destructor */
             if (cx__destructor(o)) {
                 return -1;
             }
-        } else {
-            /* Notify destruct */
+        }
+
+        /* Notify destruct (scoped objects are destructed by cx_destruct) */
+        if (!cx_checkAttr(o, CX_ATTR_SCOPED)) {
             cx_notify(cx__objectObservable(CX_OFFSET(o,-sizeof(cx__object))), o, o, CX_ON_DELETE);
         }
 
@@ -1516,7 +1509,9 @@ cx_uint16 cx__destruct(cx_object o) {
         }
 
         /* Remove from anonymous cache */
-        if (!cx_checkAttr(o, CX_ATTR_SCOPED)) {
+        if (cx_checkAttr(o, CX_ATTR_SCOPED)) {
+            cx__deinitScope(o);
+        } else {
             /* Remove from anonymous cache */
             cx_mutexLock(&cx_adminLock);
             cx_llRemove(cx_anonymousObjects, o);
