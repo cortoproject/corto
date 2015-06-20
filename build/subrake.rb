a713f260be0affe613f8849e63e967453ce2db94
jleeothon <jleeothon@gmail.com>
2015-06-20 11:43:52 -0500

    #264 Allow packages to build subpackages (1)

--- build/subrake.rb
+++ build/subrake.rb
@@ -1,10 +1,4 @@
 
-if not ENV['CORTEX_HOME'] then
-    raise "CORTEX_HOME not defined (did you forget to 'source configure'?)"
-end
-
-Dir.chdir(File.dirname(Rake.application.rakefile))
-
 if not defined? COMPONENTS then
     raise "COMPONENTS not specified\n"
 end
