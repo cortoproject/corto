e03da8c9f3ff408fe272e526176cb405e871be02
jleeothon <jleeothon@gmail.com>
2015-06-24 11:29:44 -0500

    #257 Regenerated core *.c headers
    cx.h was also regenerated.

--- cx/include/cx.h
+++ cx/include/cx.h
@@ -1,3 +1,25 @@
+/* cx.h
+ *
+ * This file contains generated code. Do not modify!
+ */
+
+#ifndef cx_H
+#define cx_H
+
+#include "cortex.h"
+#include "cx__type.h"
+#include "cx__api.h"
+#include "cx__meta.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+#endif
+
 #include "cx_alias.h"
 #include "cx_array.h"
 #include "cx_binary.h"
