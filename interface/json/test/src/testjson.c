1f50f95a203dc09b11ef3c50f28a2eed256c9ffd
jleeothon <jleeothon@gmail.com>
2015-08-13 22:23:58 -0500

    #305 Also changed testjson

---
+++ interface/json/test/src/testjson.c
@@ -0,0 +1,18 @@
+/* testjson.c
+ *
+ * This file contains the implementation for the generated interface.
+ *
+ * Don't mess with the begin and end tags, since these will ensure that modified
+ * code in interface functions isn't replaced when code is re-generated.
+ */
+
+#include "testjson.h"
+
+int testjsonmain(int argc, char* argv[]) {
+/* $begin(main) */
+    /* Insert code that must be run when component is loaded */
+    CX_UNUSED(argc);
+    CX_UNUSED(argv);
+    return 0;
+/* $end */
+}
