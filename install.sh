ac4527b6a5f15c582715a5d71603db61ee82e477
jleeothon <jleeothon@gmail.com>
2015-08-21 17:42:45 -0500

    #307 Changed not to build on client-side

--- install.sh
+++ install.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
+install_cortex_fail() {
+    >&2 echo "Installation failed."
+    rm -rf "$INSTALL_TMPDIR"
+}
+
 install_cortex () {
     set -e
-
+    set -u
     VERSION="0.2.0"
 
     # Check supported OS
@@ -11,17 +16,25 @@ install_cortex () {
         >&2 echo "Sorry, this OS is not supported yet."
         exit 1
     fi
+    ARCHITECTURE=$(uname -p)
+    if [ "$UNAME" = "Darwin" ] ; then
+        if [ "i386" != "$(uname -p)" -o "1" != "$(sysctl -n hw.cpu64bit_capable 2>/dev/null || echo 0)" ] ; then
+          >&2 echo "Only 64-bit Intel processors are supported at this time."
+          exit 1
+        fi
+    fi
 
-    trap ">&2 echo Installation failed." EXIT
+    trap install_cortex_fail EXIT
 
-    TARBALL_URL="https://codeload.github.com/cortexlang/cortex/tar.gz/master"
+    FILENAME="cortex$VERSION"
+    TARBALL_URL="https://s3.amazonaws.com/cortexbinaries/$UNAME/$ARCHITECTURE/$FILENAME.tar.gz"
     INSTALL_TMPDIR="$HOME/.cortex-install-tmp"
     rm -rf "$INSTALL_TMPDIR"
     mkdir "$INSTALL_TMPDIR"
-    echo "Downloading Cortex source code"
+    echo "Downloading Cortex source code from $TARBALL_URL."
     curl --progress-bar --fail "$TARBALL_URL" | tar -xzf - -C "$INSTALL_TMPDIR"
-    # test -d "${INSTALL_TMPDIR}/cortex-master"
-    # TODO change test
+    test -d "${INSTALL_TMPDIR}/bin"
+    test -d "${INSTALL_TMPDIR}/$VERSION"
     # The files downloaded have the structure:
     # zipped file/
     # |-cortex <- executable that goes into /usr/local/bin
@@ -30,45 +43,32 @@ install_cortex () {
     #   |-components/
     #   |-generators/
 
-    # Remove previous installation
-    echo "Removing any existing Cortex $VERSION installation."
-    sudo rm -rf "/usr/local/lib/cortex/$VERSION"
-    sudo mkdir "/usr/local/lib/cortex"
-    sudo mkdir "/usr/local/lib/cortex/$VERSION"
-    rm -rf "$HOME/.cortex/$VERSION"
-    mkdir "$HOME/.cortex"
-    mkdir "$HOME/.cortex/$VERSION"
-
-    # TODO replace cortex-master with ultimate filename
-    # Copy executable
-    cp "$INSTALL_TMPDIR/cortex-master/cortex" "/usr/local/bin/cortex/$VERSION/"
-    test -d "/usr/local/bin/cortex"
-    cp "$INSTALL_TMPDIR/cortex-master/$VERSION" "$HOME/.cortex"
-    rm -rf "${INSTALL_TMPDIR}"
-    test -d "$HOME/.cortex/$VERSION/components"
-    test -d "$HOME/.cortex/$VERSION/generators"
-    test -d "$HOME/.cortex/$VERSION/packages"
-
-    echo "The Cortex source code has been downloaded to \"~/.cortex\"."
-
-    # Build
-    cd "$HOME/.cortex"
-    source configure
-    rake
-    test -x "$HOME/.cortex/bin/cortex"
-
-    trap ">&2 echo Error building Cortex." EXIT
-
     if [ ! -d "/usr/local/bin" ] ; then
         sudo mkdir -m 755 "/usr/local" || true
         sudo mkdir -m 755 "/usr/local/bin" || true
     fi
 
-    cp "$HOME/.cortex/bin/cortex" /usr/local/bin/
-    cp "$HOME/.cortex/bin/cxgen" /usr/local/bin/
-    cp "$HOME/.cortex/bin/cxsh" /usr/local/bin/
+    echo "Removing your existing Cortex $VERSION installation."
+    sudo rm -rf "/usr/local/bin/cortex/$VERSION"
+    sudo rm -rf "/usr/local/lib/cortex/$VERSION"
+    rm -rf "$HOME/.cortex/$VERSION"
+    sudo mkdir -p "/usr/local/bin/cortex/$VERSION"
+    sudo mkdir -p "/usr/local/lib/cortex/$VERSION"
+    mkdir -p "$HOME/.cortex"
 
+    echo "Copying files."
+    # Copy basic files
+    sudo cp "$INSTALL_TMPDIR/bin/cortex" "/usr/local/bin/cortex.$VERSION"
+    sudo cp "$INSTALL_TMPDIR/bin/libcortex.so" "/usr/local/lib/libcortex.so.$VERSION"
+    # Copy interface, generator, packages
+    sudo cp -R "$INSTALL_TMPDIR/$VERSION" "/usr/local/lib/cortex/" > /dev/null
+    test -x "/usr/local/bin/cortex/$VERSION/cortex"
+    test -d "/usr/local/lib/cortex/$VERSION/interface"
+    test -d "/usr/local/lib/cortex/$VERSION/generator"
+    test -d "/usr/local/lib/cortex/$VERSION/packages"
+    rm -rf "${INSTALL_TMPDIR}"
     echo "Cortex succesfully installed."
+    trap - EXIT
 }
 
 install_cortex
