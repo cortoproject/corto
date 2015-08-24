274703c9fcbb11319876fc6147c21c1bdb449b06
jleeothon <jleeothon@gmail.com>
2015-08-24 15:31:39 -0500

    #307 Changed hardcoded path to PREFIX

--- install.sh
+++ install.sh
@@ -8,7 +8,9 @@ install_cortex_fail() {
 install_cortex () {
     set -e
     set -u
+
     VERSION="0.2.0"
+    PREFIX="/usr"
 
     # Check supported OS
     UNAME=$(uname)
@@ -37,35 +39,39 @@ install_cortex () {
     test -d "${INSTALL_TMPDIR}/$VERSION"
     # The files downloaded have the structure:
     # zipped file/
-    # |-cortex <- executable that goes into /usr/local/bin
+    # |-cortex <- executable that goes into $PREFIX/bin
     # |-0.0.0/ <- directory named after version number
     #   |-packages/
     #   |-components/
     #   |-generators/
 
-    if [ ! -d "/usr/local/bin" ] ; then
-        sudo mkdir -m 755 "/usr/local" || true
-        sudo mkdir -m 755 "/usr/local/bin" || true
+    if [ ! -d "$PREFIX/bin" ] ; then
+        sudo mkdir -m 755 "$PREFIX" || true
+        sudo mkdir -m 755 "$PREFIX/bin" || true
     fi
 
     echo "Removing your existing Cortex $VERSION installation."
-    sudo rm -rf "/usr/local/bin/cortex/$VERSION"
-    sudo rm -rf "/usr/local/lib/cortex/$VERSION"
+    sudo rm -rf "$PREFIX/lib/cortex/$VERSION"
     rm -rf "$HOME/.cortex/$VERSION"
-    sudo mkdir -p "/usr/local/bin/cortex/$VERSION"
-    sudo mkdir -p "/usr/local/lib/cortex/$VERSION"
+    sudo mkdir -p "$PREFIX/lib/cortex/$VERSION"
     mkdir -p "$HOME/.cortex"
 
     echo "Copying files."
     # Copy basic files
-    sudo cp "$INSTALL_TMPDIR/bin/cortex" "/usr/local/bin/cortex.$VERSION"
-    sudo cp "$INSTALL_TMPDIR/bin/libcortex.so" "/usr/local/lib/libcortex.so.$VERSION"
+    sudo rm "$PREFIX/bin/cortex" || true
+    sudo rm "$PREFIX/bin/cortex.$VERSION" || true
+    sudo rm "$PREFIX/lib/libcortex.so" || true
+    sudo rm "$PREFIX/lib/libcortex.so.$VERSION" || true
+    sudo cp "$INSTALL_TMPDIR/bin/cortex" "$PREFIX/bin/cortex.$VERSION"
+    sudo cp "$INSTALL_TMPDIR/bin/libcortex.so" "$PREFIX/lib/libcortex.so.$VERSION"
+    sudo ln -s "$PREFIX/bin/cortex.$VERSION" "$PREFIX/bin/cortex"
+    sudo ln -s "$PREFIX/lib/libcortex.so.$VERSION" "$PREFIX/lib/libcortex.so"
     # Copy interface, generator, packages
-    sudo cp -R "$INSTALL_TMPDIR/$VERSION" "/usr/local/lib/cortex/" > /dev/null
-    test -x "/usr/local/bin/cortex/$VERSION/cortex"
-    test -d "/usr/local/lib/cortex/$VERSION/interface"
-    test -d "/usr/local/lib/cortex/$VERSION/generator"
-    test -d "/usr/local/lib/cortex/$VERSION/packages"
+    sudo cp -R "$INSTALL_TMPDIR/$VERSION" "$PREFIX/lib/cortex/" > /dev/null
+    test -x "$PREFIX/bin/cortex.$VERSION"
+    test -d "$PREFIX/lib/cortex/$VERSION/interface"
+    test -d "$PREFIX/lib/cortex/$VERSION/generator"
+    test -d "$PREFIX/lib/cortex/$VERSION/packages"
     rm -rf "${INSTALL_TMPDIR}"
     echo "Cortex succesfully installed."
     trap - EXIT
