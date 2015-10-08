84631ceceb0cf77971058a500ce4e76ef3a5c32d
Sander Mertens <sander.mertens8@gmail.com>
2015-10-07 21:35:25 -0400

    #16 Add support for paragraphs

---
+++ packages/corto/md/test/include/test_HeadersParagraphs.h
@@ -0,0 +1,25 @@
+/* test_HeadersParagraphs.h
+ *
+ * This file contains generated code. Do not modify!
+ */
+
+#ifndef TEST_HEADERSPARAGRAPHS_H
+#define TEST_HEADERSPARAGRAPHS_H
+
+#include "corto.h"
+#include "corto/test/test_Suite.h"
+#include "test__interface.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* ::test::HeadersParagraphs::tc_H1() */
+TEST_EXPORT cx_void _test_HeadersParagraphs_tc_H1(test_HeadersParagraphs _this);
+#define test_HeadersParagraphs_tc_H1(_this) _test_HeadersParagraphs_tc_H1(test_HeadersParagraphs(_this))
+
+#ifdef __cplusplus
+}
+#endif
+#endif
+
---
+++ packages/corto/md/test/src/test_HeaderParagraphs.c
@@ -0,0 +1,433 @@
+/* test_HeaderParagraphs.c
+ *
+ * This file contains the implementation for the generated interface.
+ *
+ * Don't mess with the begin and end tags, since these will ensure that modified
+ * code in interface functions isn't replaced when code is re-generated.
+ */
+
+#include "test.h"
+
+/* ::test::HeaderParagraphs::tc_H1H2H3H4P() */
+cx_void _test_HeaderParagraphs_tc_H1H2H3H4P(test_HeaderParagraphs this) {
+/* $begin(::test::HeaderParagraphs::tc_H1H2H3H4P) */
+
+    char text[] =
+        "# FirstHeading\n"
+        "## SecondHeading\n"
+        "### ThirdHeading\n"
+        "#### FourthHeading\n"
+        "This is the first paragraph.\n"
+        ;
+    md_parse(test_docs_o, text);
+    {
+        cx_object o = cx_resolve(test_docs_o, "FirstHeading");
+        test_assert(o != NULL);
+        test_assert(cx_instanceof(md_Doc_o, o));
+        test_assert(md_Doc(o)->o == NULL);
+        test_assert(md_Doc(o)->text == NULL);
+        test_assert(md_Doc(o)->level == 1);
+        cx_release(o);
+    }
+    {
+        cx_object o = cx_resolve(test_docs_o, "FirstHeading::SecondHeading");
+        test_assert(o != NULL);
+        test_assert(cx_instanceof(md_Doc_o, o));
+        test_assert(md_Doc(o)->o == NULL);
+        test_assert(md_Doc(o)->text == NULL);
+        test_assert(md_Doc(o)->level == 2);
+        cx_release(o);
+    }
+    {
+        cx_object o = cx_resolve(test_docs_o, "FirstHeading::SecondHeading::ThirdHeading");
+        test_assert(o != NULL);
+        test_assert(cx_instanceof(md_Doc_o, o));
+        test_assert(md_Doc(o)->o == NULL);
+        test_assert(md_Doc(o)->text == NULL);
+        test_assert(md_Doc(o)->level == 3);
+        cx_release(o);
+    }
+    {
+        cx_object o = cx_resolve(test_docs_o, "FirstHeading::SecondHeading::ThirdHeading::FourthHeading");
+        test_assert(o != NULL);
+        test_assert(cx_instanceof(md_Doc_o, o));
+        test_assert(md_Doc(o)->o == NULL);
+        test_assert(md_Doc(o)->text != NULL);
+        test_assert(!strcmp(md_Doc(o)->text, "This is the first paragraph.\n"));
+        test_assert(md_Doc(o)->level == 4);
+        cx_release(o);
+    }
+
+/* $end */
+}
+
+/* ::test::HeaderParagraphs::tc_H1H2H3P() */
+cx_void _test_HeaderParagraphs_tc_H1H2H3P(test_HeaderParagraphs this) {
+/* $begin(::test::HeaderParagraphs::tc_H1H2H3P) */
+
+    char text[] =
+        "# FirstHeading\n"
+        "## SecondHeading\n"
+        "### ThirdHeading\n"
+        "This is the first paragraph.\n"
+        ;
+    md_parse(test_docs_o, text);
+    {
+        cx_object o = cx_resolve(test_docs_o, "FirstHeading");
+        test_assert(o != NULL);
+        test_assert(cx_instanceof(md_Doc_o, o));
+        test_assert(md_Doc(o)->o == NULL);
+        test_assert(md_Doc(o)->text == NULL);
+        test_assert(md_Doc(o)->level == 1);
+        cx_release(o);
+    }
+    {
+        cx_object o = cx_resolve(test_docs_o, "FirstHeading::SecondHeading");
+        test_assert(o != NULL);
+        test_assert(cx_instanceof(md_Doc_o, o));
+        test_assert(md_Doc(o)->o == NULL);
+        test_assert(md_Doc(o)->text == NULL);
+        test_assert(md_Doc(o)->level == 2);
+        cx_release(o);
+    }
+    {
+        cx_object o = cx_resolve(test_docs_o, "FirstHeading::SecondHeading::ThirdHeading");
+        test_assert(o != NULL);
+        test_assert(cx_instanceof(md_Doc_o, o));
+        test_assert(md_Doc(o)->o == NULL);
+        test_assert(md_Doc(o)->text != NULL);
+        test_assert(!strcmp(md_Doc(o)->text, "This is the first paragraph.\n"));
+        test_assert(md_Doc(o)->level == 3);
+        cx_release(o);
+    }
+
+/* $end */
+}
+
+/* ::test::HeaderParagraphs::tc_H1H4P() */
+cx_void _test_HeaderParagraphs_tc_H1H4P(test_HeaderParagraphs this) {
+/* $begin(::test::HeaderParagraphs::tc_H1H4P) */
+
+    char text[] =
+        "# FirstHeading\n"
+        "#### SecondHeading\n"
+        "This is the first paragraph.\n"
+        ;
+    md_parse(test_docs_o, text);
+    {
+        cx_object o = cx_resolve(test_docs_o, "FirstHeading");
+        test_assert(o != NULL);
+        test_assert(cx_instanceof(md_Doc_o, o));
+        test_assert(md_Doc(o)->o == NULL);
+        test_assert(md_Doc(o)->text == NULL);
+        test_assert(md_Doc(o)->level == 1);
+        cx_release(o);
+    }
+    {
+        cx_object o = cx_resolve(test_docs_o, "FirstHeading::SecondHeading");
+        test_assert(o != NULL);
+        test_assert(cx_instanceof(md_Doc_o, o));
+        test_assert(md_Doc(o)->o == NULL);
+        test_assert(md_Doc(o)->text != NULL);
+        test_assert(!strcmp(md_Doc(o)->text, "This is the first paragraph.\n"));
+        test_assert(md_Doc(o)->level == 4);
+        cx_release(o);
+    }
+
+/* $end */
+}
+
+/* ::test::HeaderParagraphs::tc_H1P() */
+cx_void _test_HeaderParagraphs_tc_H1P(test_HeaderParagraphs this) {
+/* $begin(::test::HeaderParagraphs::tc_H1P) */
+    char text[] =
+        "# FirstHeading\n"
+        "This is the first paragraph.\n"
+        ;
+    md_parse(test_docs_o, text);
+    {
+        cx_object o = cx_resolve(test_docs_o, "FirstHeading");
+        test_assert(o != NULL);
+        test_assert(cx_instanceof(md_Doc_o, o));
+        test_assert(md_Doc(o)->o == NULL);
+        test_assert(md_Doc(o)->text != NULL);
+        test_assert(!strcmp(md_Doc(o)->text, "This is the first paragraph.\n"));
+        test_assert(md_Doc(o)->level == 1);
+        cx_release(o);
+    }
+/* $end */
+}
+
+/* ::test::HeaderParagraphs::tc_H1PH1P() */
+cx_void _test_HeaderParagraphs_tc_H1PH1P(test_HeaderParagraphs this) {
+/* $begin(::test::HeaderParagraphs::tc_H1PH1P) */
+
+    char text[] =
+        "# FirstHeading\n"
+        "This is the first paragraph.\n"
+        "# SecondHeading\n"
+        "This is the second paragraph.\n";
+        ;
+    md_parse(test_docs_o, text);
+    {
+        cx_object o = cx_resolve(test_docs_o, "FirstHeading");
+        test_assert(o != NULL);
+        test_assert(cx_instanceof(md_Doc_o, o));
+        test_assert(md_Doc(o)->o == NULL);
+        test_assert(md_Doc(o)->text != NULL);
+        test_assert(!strcmp(md_Doc(o)->text, "This is the first paragraph.\n"));
+        test_assert(md_Doc(o)->level == 1);
+        cx_release(o);
+    }
+    {
+        cx_object o = cx_resolve(test_docs_o, "SecondHeading");
+        test_assert(o != NULL);
+        test_assert(cx_instanceof(md_Doc_o, o));
+        test_assert(md_Doc(o)->o == NULL);
+        test_assert(md_Doc(o)->text != NULL);
+        test_assert(!strcmp(md_Doc(o)->text, "This is the second paragraph.\n"));
+        test_assert(md_Doc(o)->level == 1);
+        cx_release(o);
+    }
+
+/* $end */
+}
+
+/* ::test::HeaderParagraphs::tc_H1PH2P() */
+cx_void _test_HeaderParagraphs_tc_H1PH2P(test_HeaderParagraphs this) {
+/* $begin(::test::HeaderParagraphs::tc_H1PH2P) */
+
+    char text[] =
+        "# FirstHeading\n"
+        "This is the first paragraph.\n"
+        "## SecondHeading\n"
+        "This is the second paragraph.\n";
+        ;
+    md_parse(test_docs_o, text);
+    {
+        cx_object o = cx_resolve(test_docs_o, "FirstHeading");
+        test_assert(o != NULL);
+        test_assert(cx_instanceof(md_Doc_o, o));
+        test_assert(md_Doc(o)->o == NULL);
+        test_assert(md_Doc(o)->text != NULL);
+        test_assert(!strcmp(md_Doc(o)->text, "This is the first paragraph.\n"));
+        test_assert(md_Doc(o)->level == 1);
+        cx_release(o);
+    }
+    {
+        cx_object o = cx_resolve(test_docs_o, "FirstHeading::SecondHeading");
+        test_assert(o != NULL);
+        test_assert(cx_instanceof(md_Doc_o, o));
+        test_assert(md_Doc(o)->o == NULL);
+        test_assert(md_Doc(o)->text != NULL);
+        test_assert(!strcmp(md_Doc(o)->text, "This is the second paragraph.\n"));
+        test_assert(md_Doc(o)->level == 2);
+        cx_release(o);
+    }
+
+/* $end */
+}
+
+/* ::test::HeaderParagraphs::tc_H1PH2PH3P() */
+cx_void _test_HeaderParagraphs_tc_H1PH2PH3P(test_HeaderParagraphs this) {
+/* $begin(::test::HeaderParagraphs::tc_H1PH2PH3P) */
+
+    char text[] =
+        "# FirstHeading\n"
+        "This is the first paragraph.\n"
+        "## SecondHeading\n"
+        "This is the second paragraph.\n"
+        "### ThirdHeading\n"
+        "This is the third paragraph.\n";
+        ;
+    md_parse(test_docs_o, text);
+    {
+        cx_object o = cx_resolve(test_docs_o, "FirstHeading");
+        test_assert(o != NULL);
+        test_assert(cx_instanceof(md_Doc_o, o));
+        test_assert(md_Doc(o)->o == NULL);
+        test_assert(md_Doc(o)->text != NULL);
+        test_assert(!strcmp(md_Doc(o)->text, "This is the first paragraph.\n"));
+        test_assert(md_Doc(o)->level == 1);
+        cx_release(o);
+    }
+    {
+        cx_object o = cx_resolve(test_docs_o, "FirstHeading::SecondHeading");
+        test_assert(o != NULL);
+        test_assert(cx_instanceof(md_Doc_o, o));
+        test_assert(md_Doc(o)->o == NULL);
+        test_assert(md_Doc(o)->text != NULL);
+        test_assert(!strcmp(md_Doc(o)->text, "This is the second paragraph.\n"));
+        test_assert(md_Doc(o)->level == 2);
+        cx_release(o);
+    }
+    {
+        cx_object o = cx_resolve(test_docs_o, "FirstHeading::SecondHeading::ThirdHeading");
+        test_assert(o != NULL);
+        test_assert(cx_instanceof(md_Doc_o, o));
+        test_assert(md_Doc(o)->o == NULL);
+        test_assert(md_Doc(o)->text != NULL);
+        test_assert(!strcmp(md_Doc(o)->text, "This is the third paragraph.\n"));
+        test_assert(md_Doc(o)->level == 3);
+        cx_release(o);
+    }
+
+/* $end */
+}
+
+/* ::test::HeaderParagraphs::tc_H1PH3P() */
+cx_void _test_HeaderParagraphs_tc_H1PH3P(test_HeaderParagraphs this) {
+/* $begin(::test::HeaderParagraphs::tc_H1PH3P) */
+
+    char text[] =
+        "# FirstHeading\n"
+        "This is the first paragraph.\n"
+        "### SecondHeading\n"
+        "This is the second paragraph.\n";
+        ;
+    md_parse(test_docs_o, text);
+    {
+        cx_object o = cx_resolve(test_docs_o, "FirstHeading");
+        test_assert(o != NULL);
+        test_assert(cx_instanceof(md_Doc_o, o));
+        test_assert(md_Doc(o)->o == NULL);
+        test_assert(md_Doc(o)->text != NULL);
+        test_assert(!strcmp(md_Doc(o)->text, "This is the first paragraph.\n"));
+        test_assert(md_Doc(o)->level == 1);
+        cx_release(o);
+    }
+    {
+        cx_object o = cx_resolve(test_docs_o, "FirstHeading::SecondHeading");
+        test_assert(o != NULL);
+        test_assert(cx_instanceof(md_Doc_o, o));
+        test_assert(md_Doc(o)->o == NULL);
+        test_assert(md_Doc(o)->text != NULL);
+        test_assert(!strcmp(md_Doc(o)->text, "This is the second paragraph.\n"));
+        test_assert(md_Doc(o)->level == 3);
+        cx_release(o);
+    }
+
+/* $end */
+}
+
+/* ::test::HeaderParagraphs::tc_H1PnH1P() */
+cx_void _test_HeaderParagraphs_tc_H1PnH1P(test_HeaderParagraphs this) {
+/* $begin(::test::HeaderParagraphs::tc_H1PnH1P) */
+
+    char text[] =
+        "# FirstHeading\n"
+        "This is the first paragraph.\n\n"
+        "# SecondHeading\n"
+        "This is the second paragraph.\n";
+        ;
+    md_parse(test_docs_o, text);
+    {
+        cx_object o = cx_resolve(test_docs_o, "FirstHeading");
+        test_assert(o != NULL);
+        test_assert(cx_instanceof(md_Doc_o, o));
+        test_assert(md_Doc(o)->o == NULL);
+        test_assert(md_Doc(o)->text != NULL);
+        test_assert(!strcmp(md_Doc(o)->text, "This is the first paragraph.\n"));
+        test_assert(md_Doc(o)->level == 1);
+        cx_release(o);
+    }
+    {
+        cx_object o = cx_resolve(test_docs_o, "SecondHeading");
+        test_assert(o != NULL);
+        test_assert(cx_instanceof(md_Doc_o, o));
+        test_assert(md_Doc(o)->o == NULL);
+        test_assert(md_Doc(o)->text != NULL);
+        test_assert(!strcmp(md_Doc(o)->text, "This is the second paragraph.\n"));
+        test_assert(md_Doc(o)->level == 1);
+        cx_release(o);
+    }
+
+/* $end */
+}
+
+/* ::test::HeaderParagraphs::tc_H1PnH2P() */
+cx_void _test_HeaderParagraphs_tc_H1PnH2P(test_HeaderParagraphs this) {
+/* $begin(::test::HeaderParagraphs::tc_H1PnH2P) */
+
+    char text[] =
+        "# FirstHeading\n"
+        "This is the first paragraph.\n\n"
+        "## SecondHeading\n"
+        "This is the second paragraph.\n";
+        ;
+    md_parse(test_docs_o, text);
+    {
+        cx_object o = cx_resolve(test_docs_o, "FirstHeading");
+        test_assert(o != NULL);
+        test_assert(cx_instanceof(md_Doc_o, o));
+        test_assert(md_Doc(o)->o == NULL);
+        test_assert(md_Doc(o)->text != NULL);
+        test_assert(!strcmp(md_Doc(o)->text, "This is the first paragraph.\n"));
+        test_assert(md_Doc(o)->level == 1);
+        cx_release(o);
+    }
+    {
+        cx_object o = cx_resolve(test_docs_o, "FirstHeading::SecondHeading");
+        test_assert(o != NULL);
+        test_assert(cx_instanceof(md_Doc_o, o));
+        test_assert(md_Doc(o)->o == NULL);
+        test_assert(md_Doc(o)->text != NULL);
+        test_assert(!strcmp(md_Doc(o)->text, "This is the second paragraph.\n"));
+        test_assert(md_Doc(o)->level == 2);
+        cx_release(o);
+    }
+
+/* $end */
+}
+
+/* ::test::HeaderParagraphs::tc_H1PP() */
+cx_void _test_HeaderParagraphs_tc_H1PP(test_HeaderParagraphs this) {
+/* $begin(::test::HeaderParagraphs::tc_H1PP) */
+
+    char text[] =
+        "# FirstHeading\n"
+        "This is the first paragraph.\n"
+        "This is the second paragraph.\n";
+        ;
+    md_parse(test_docs_o, text);
+    {
+        cx_object o = cx_resolve(test_docs_o, "FirstHeading");
+        test_assert(o != NULL);
+        test_assert(cx_instanceof(md_Doc_o, o));
+        test_assert(md_Doc(o)->o == NULL);
+        test_assert(md_Doc(o)->text != NULL);
+        test_assert(!strcmp(md_Doc(o)->text, "This is the first paragraph.\nThis is the second paragraph.\n"));
+        test_assert(md_Doc(o)->level == 1);
+        cx_release(o);
+    }
+
+/* $end */
+}
+
+/* ::test::HeaderParagraphs::tc_H1PPP() */
+cx_void _test_HeaderParagraphs_tc_H1PPP(test_HeaderParagraphs this) {
+/* $begin(::test::HeaderParagraphs::tc_H1PPP) */
+
+    char text[] =
+        "# FirstHeading\n"
+        "This is the first paragraph.\n"
+        "This is the second paragraph.\n"
+        "This is the third paragraph.\n";
+        ;
+    md_parse(test_docs_o, text);
+    {
+        cx_object o = cx_resolve(test_docs_o, "FirstHeading");
+        test_assert(o != NULL);
+        test_assert(cx_instanceof(md_Doc_o, o));
+        test_assert(md_Doc(o)->o == NULL);
+        test_assert(md_Doc(o)->text != NULL);
+        test_assert(!strcmp(md_Doc(o)->text, 
+            "This is the first paragraph.\n"
+            "This is the second paragraph.\n"
+            "This is the third paragraph.\n"));
+        test_assert(md_Doc(o)->level == 1);
+        cx_release(o);
+    }
+
+/* $end */
+}
