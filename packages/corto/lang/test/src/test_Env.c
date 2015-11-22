962bc2e1171017a26da60152d39211bdef6f6658
jleeothon <jleeothon@gmail.com>
2015-11-22 16:30:49 +0100

    The build system moved a function around

--- packages/corto/lang/test/src/test_Env.c
+++ packages/corto/lang/test/src/test_Env.c
@@ -35,14 +35,6 @@ corto_void _test_Env_tc_envparseReplaceWithSameVariable(test_Env this) {
 /* $end */
 }
 
-corto_void _test_Env_tc_setenvSimple(test_Env this) {
-/* $begin(test/Env/tc_setenvSimple) */
-    corto_setenv("CORTO_TEST_A", "A");
-    char* a = corto_getenv("CORTO_TEST_A");
-    test_assert(!strcmp(a, "A"));
-/* $end */
-}
-
 corto_void _test_Env_tc_envparseSimpleFormat(test_Env this) {
 /* $begin(test/Env/tc_envparseSimpleFormat) */
     char* s = corto_envparse("A %s", "B");
@@ -64,5 +56,13 @@ corto_void _test_Env_tc_envparseVariableNotFound(test_Env this) {
 /* $begin(test/Env/tc_envparseVariableNotFound) */
     char* s = corto_envparse("ABC ABC $CORTO_NOT_FOUND");
     test_assert(s == NULL);
+/* $end */
+}
+
+corto_void _test_Env_tc_setenvSimple(test_Env this) {
+/* $begin(test/Env/tc_setenvSimple) */
+    corto_setenv("CORTO_TEST_A", "A");
+    char* a = corto_getenv("CORTO_TEST_A");
+    test_assert(!strcmp(a, "A"));
 /* $end */
 }
