04405750eb803dd244548842bc3de7ba0bcf636c
Sander Mertens <sander.mertens8@gmail.com>
2016-07-14 17:32:30 -0700

    #473 remove warning on ARM
    Cleanup test code of attrtest

--- packages/corto/test/corto/attrtest/src/attrtest.c
+++ packages/corto/test/corto/attrtest/src/attrtest.c
@@ -1,11 +1,6 @@
 
 #include "attrtest/attrtest.h"
 
-/* Add implementation here */
-
-/* cortomain is called when the package is loaded by Corto. It is a good
- * place for initialization code that needs to be executed once.
- * The function can be safely removed if not needed. */
 int attrtestMain(int argc, char *argv[]) {
     return corto_getAttr() != CORTO_ATTR_DEFAULT;
 }
