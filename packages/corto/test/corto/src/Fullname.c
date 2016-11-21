a607a14c6fb3f8168dc7f68be9dbcf8adb634fec
Sander Mertens <sander.mertens8@gmail.com>
2016-11-21 02:36:21 -0800

    #341 add testcases for anonymous objects and corto_fullpath

--- packages/corto/test/corto/src/Fullname.c
+++ packages/corto/test/corto/src/Fullname.c
@@ -8,6 +8,75 @@
 
 #include <test.h>
 
+corto_void _test_Fullname_tc_anonymousAnonymous(
+    test_Fullname this)
+{
+/* $begin(test/Fullname/tc_anonymousAnonymous) */
+    corto_object o = corto_resolve(NULL, "list{int32}{10, 20, 30}");
+    test_assert(o != NULL);
+    test_assert(corto_typeof(o)->kind == CORTO_COLLECTION);
+    corto_ll *l = o;
+    test_assertint(corto_llSize(*l), 3);
+
+    test_assertstr(corto_fullpath(NULL, o), "list{int32,0}{10,20,30}");
+    corto_int16 ret = corto_delete(o);
+    test_assert(ret == 0);
+
+/* $end */
+}
+
+corto_void _test_Fullname_tc_anonymousCollection(
+    test_Fullname this)
+{
+/* $begin(test/Fullname/tc_anonymousCollection) */
+    corto_object o = corto_resolve(NULL, "test/PrimitiveList{10, 20, 30}");
+    test_assert(o != NULL);
+    test_assert(corto_typeof(o) == corto_type(test_PrimitiveList_o));
+    test_PrimitiveList *l = test_PrimitiveList(o);
+    test_assertint(corto_llSize(*l), 3);
+
+    test_assertstr(corto_fullpath(NULL, o), "/test/PrimitiveList{10,20,30}");
+    corto_int16 ret = corto_delete(o);
+    test_assert(ret == 0);
+
+/* $end */
+}
+
+corto_void _test_Fullname_tc_anonymousComposite(
+    test_Fullname this)
+{
+/* $begin(test/Fullname/tc_anonymousComposite) */
+    corto_object o = corto_resolve(NULL, "test/Point{10, 20}");
+    test_assert(o != NULL);
+    test_assert(corto_typeof(o) == corto_type(test_Point_o));
+    test_Point *p = test_Point(o);
+    test_assertint(p->x, 10);
+    test_assertint(p->y, 20);
+
+    test_assertstr(corto_fullpath(NULL, o), "/test/Point{10,20}");
+    corto_int16 ret = corto_delete(o);
+    test_assert(ret == 0);
+
+/* $end */
+}
+
+corto_void _test_Fullname_tc_anonymousPrimitive(
+    test_Fullname this)
+{
+/* $begin(test/Fullname/tc_anonymousPrimitive) */
+    corto_object o = corto_resolve(NULL, "int32{10}");
+    test_assert(o != NULL);
+    test_assert(corto_typeof(o) == corto_type(corto_int32_o));
+    corto_int32 *p = o;
+    test_assertint(*p, 10);
+
+    test_assertstr(corto_fullpath(NULL, o), "int32{10}");
+    corto_int16 ret = corto_delete(o);
+    test_assert(ret == 0);
+
+/* $end */
+}
+
 corto_void _test_Fullname_tc_fromLang(
     test_Fullname this)
 {
