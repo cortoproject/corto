3d213132476a616d46b42d285438c7df9932937e
Sander Mertens <sander.mertens8@gmail.com>
2017-01-31 18:54:21 -0800

    #341 remove redundant files

--- packages/corto/lib/everywhere/core/c/build.rb
+++
@@ -1,2 +0,0 @@
-INCLUDE << "src" << "/home/vagrant/.corto/include/corto/0.2" << "/usr/local/include/corto/0.2"
-LINK << "/home/vagrant/.corto/lib/corto/0.2/corto/core/c/c" << "/home/vagrant/.corto/lib/corto/0.2/corto/corto"
