0b90bcd6de4b943dd1a7399169624edafb50f008
Sander Mertens <sander.mertens8@gmail.com>
2018-02-18 17:24:41 -0800

    #341 fix platform test

--- test/platform/include/test.h
+++ test/platform/include/test.h
@@ -1,5 +1,4 @@
 /* test.h
- *
  * This is the main package file. Include this file in other projects.
  * Only modify inside the header-end and body-end sections.
  */
@@ -7,7 +6,6 @@
 #ifndef TEST_H
 #define TEST_H
 
-#include <corto/corto.h>
 #include <corto/corto.h>
 #include <include/_project.h>
 #include <corto/test/c/c.h>
@@ -21,6 +19,7 @@
 #include <include/_type.h>
 #include <include/_interface.h>
 #include <include/_load.h>
+#include <include/_binding.h>
 #include <include/_api.h>
 
 /* $body() */
